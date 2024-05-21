bd16b1284f43c9c67928c93c0f6a262895fc5dcb
mats cronqvist <masse@cronqvi.st>
2024-05-21 07:27:58 +0000

    tweak

--- erlang-ts-mode.el
+++ erlang-ts-mode.el
@@ -1,9 +1,20 @@
-(require 'treesit nil t)
-(unless (featurep 'treesit)
-  (error "trErl requires tree-sitter to be installed"))
+;;; trerl --- major mode for Erlang.
+;;; Commentary:
+;;; The canonical `erlang-mode' is as of this writing 30 years
+;;; old.  This is a from-scratch mode based on tree-sitter, xref, and
+;;; autocomplete.
+;;; Code:
 
-(unless (and (version<= "29.1" emacs-version) (treesit-language-available-p 'erlang))
-  (add-to-list 'treesit-language-source-alist (cons 'erlang '("https://github.com/WhatsApp/tree-sitter-erlang")))
+(require 'treesit)
+(unless (featurep 'treesit)
+  (error "TrErl requires tree-sitter to be installed"))
+
+(defvar treesit-language-source-alist)
+(unless (treesit-language-available-p 'erlang)
+  (add-to-list
+   'treesit-language-source-alist
+   (cons 'erlang '("https://github.com/WhatsApp/tree-sitter-erlang")))
+
   (treesit-install-language-grammar 'erlang))
 
 (defgroup erlang-ts nil
@@ -11,112 +22,91 @@
   :group 'languages
   :prefix "erlang-ts-")
 
-(require 'erlang-ts-fontlock)
-(require 'erlang-ts-indent)
-(require 'erlang-ts-imenu)
-(require 'erlang-ts-defun)
+;;(require 'trerl-fontlock nil t)
+(require 'trerl-indent nil t)
+(require 'trerl-imenu nil t)
+(require 'trerl-defun nil t)
 
-(defun erlang-ts-setup()
-  (when (treesit-ready-p 'erlang)
-    (treesit-parser-create 'erlang)
+;;(treesit-parser-create 'erlang)
+(defvar trerl-font-lock-rules
+  '(:language erlang :override t :feature
+              comment
+              ((comment) @font-lock-comment-face)
+              :language erlang :override t :feature
+              string
+              ((string) @font-lock-string-face)
+              :language erlang :override t :feature
+              variable-name
+              ((var) @font-lock-variable-name-face)
+              :language erlang :override nil :feature
+              constant
+              ((atom) @font-lock-constant-face)))
+))
 
-    ;; treesit-font-lock-settings
-    (erlang-ts-treesit-font-lock-settings-setup)
+(defvar trerl-font-lock-feature-list
+  '((comment string doc) ; level 1
+    (function-name keyword type builtin constant) ; level 2
+    (variable-name string-interpolation key))) ; level 3
 
-    ;; treesit-simple-indent-rules
-    (erlang-ts-treesit-simple-indent-rules-setup)
-
-    ;; treesit-defun-type-regexp
-    (erlang-ts-treesit-defun-type-regexp-setup)
-
-    ;; treesit-defun-name-function
-    (erlang-ts-treesit-defun-name-function-setup)
-
-    ;; treesit-simple-imenu-settings
-    (erlang-ts-treesit-simple-imenu-settings-setup)
-
-    (treesit-major-mode-setup)))
+(defun trerl-setup()
+  "Initialize."
+  (setq-local
+   treesit-simple-indent-rules ()
+   treesit-defun-type-regexp ()
+   treesit-defun-name-function ()
+   treesit-simple-imenu-settings ()
+   treesit-font-lock-settings (apply #'treesit-font-lock-rules trerl-font-lock-rules)
+   treesit-font-lock-feature-list trerl-font-lock-feature-list)
+  (treesit-major-mode-setup))
 
 (defvar erlang-ts-mode-map
   ;; Similar bindings as erlang-mode
   (let ((map (make-sparse-keymap)))
-    ; (define-key map ";"       'erlang-ts-electric-semicolon)
-    ; (define-key map ","       'erlang-ts-electric-comma)
-    ; (define-key map "<"         'erlang-ts-electric-lt)
-    ; (define-key map ">"         'erlang-ts-electric-gt)
-    ; (define-key map "\C-m"      'erlang-ts-electric-newline)
+    ;; (define-key map ";"       'trerl-electric-semicolon)
+    ;; (define-key map ","       'trerl-electric-comma)
+    ;; (define-key map "<"         'trerl-electric-lt)
+    ;; (define-key map ">"         'trerl-electric-gt)
+    ;; (define-key map "\C-m"      'trerl-electric-newline)
     (define-key map (kbd "DEL") 'backward-delete-char-untabify)
-    ; (define-key map "\M-q"      'erlang-ts-fill-paragraph)
-    ; (define-key map "\M-\t"     'erlang-ts-complete-tag)
-    ; (define-key map "\C-c\M-\t" 'tempo-complete-tag)
-    ; (define-key map "\M-+"      'erlang-ts-find-next-tag)
-    ; (define-key map "\C-c\M-a"  'erlang-ts-beginning-of-clause)
-    ; (define-key map "\C-c\M-b"  'tempo-backward-mark)
-    ; (define-key map "\C-c\M-e"  'erlang-ts-end-of-clause)
-    ; (define-key map "\C-c\M-f"  'tempo-forward-mark)
-    ; (define-key map "\C-c\M-h"  'erlang-ts-mark-clause)
+    ;; (define-key map "\M-q"      'trerl-fill-paragraph)
+    ;; (define-key map "\M-\t"     'trerl-complete-tag)
+    ;; (define-key map "\C-c\M-\t" 'tempo-complete-tag)
+    ;; (define-key map "\M-+"      'trerl-find-next-tag)
+    ;; (define-key map "\C-c\M-a"  'trerl-beginning-of-clause)
+    ;; (define-key map "\C-c\M-b"  'tempo-backward-mark)
+    ;; (define-key map "\C-c\M-e"  'trerl-end-of-clause)
+    ;; (define-key map "\C-c\M-f"  'tempo-forward-mark)
+    ;; (define-key map "\C-c\M-h"  'trerl-mark-clause)
     (define-key map "\C-c\C-c"  'comment-region)
-    ; (define-key map "\C-c\C-j"  'erlang-ts-generate-new-clause)
-    ; (define-key map "\C-c\C-k"  'erlang-ts-compile)
-    ; (define-key map "\C-c\C-l"  'erlang-ts-compile-display)
-    ; (define-key map "\C-c\C-s"  'erlang-ts-show-syntactic-information)
-    ; (define-key map "\C-c\C-q"  'erlang-ts-indent-function)
+    ;; (define-key map "\C-c\C-j"  'trerl-generate-new-clause)
+    ;; (define-key map "\C-c\C-k"  'trerl-compile)
+    ;; (define-key map "\C-c\C-l"  'trerl-compile-display)
+    ;; (define-key map "\C-c\C-s"  'trerl-show-syntactic-information)
+    ;; (define-key map "\C-c\C-q"  'trerl-indent-function)
     (define-key map "\C-c\C-u"  'uncomment-region)
-    ; (define-key map "\C-c\C-y"  'erlang-ts-clone-arguments)
-    ; (define-key map "\C-c\C-a"  'erlang-ts-align-arrows)
-    ; (define-key map "\C-c\C-z"  'erlang-ts-shell-display)
-    ; (define-key map "\C-c\C-d"  'erlang-ts-man-function-no-prompt)
+    ;; (define-key map "\C-c\C-y"  'trerl-clone-arguments)
+    ;; (define-key map "\C-c\C-a"  'trerl-align-arrows)
+    ;; (define-key map "\C-c\C-z"  'trerl-shell-display)
+    ;; (define-key map "\C-c\C-d"  'trerl-man-function-no-prompt)
     map)
   "Keymap for trErl.")
 
-(defvar erlang-ts-mode-syntax-table
-  ;; Import from erlang-mode
-  (let ((table (make-syntax-table)))
-      (modify-syntax-entry ?\n ">" table)
-      (modify-syntax-entry ?\" "\"" table)
-      (modify-syntax-entry ?# "." table)
-      ;; (modify-syntax-entry ?$ "\\" table)   ;; Creates problems with indentation afterwards
-      ;; (modify-syntax-entry ?$ "'" table)    ;; Creates syntax highlighting and indentation problems
-      (modify-syntax-entry ?$ "/" table)    ;; Misses the corner case "string that ends with $"
-      ;; we have to live with that for now..it is the best alternative
-      ;; that can be worked around with "string that ends with \$"
-      (modify-syntax-entry ?% "<" table)
-      (modify-syntax-entry ?& "." table)
-      (modify-syntax-entry ?\' "\"" table)
-      (modify-syntax-entry ?* "." table)
-      (modify-syntax-entry ?+ "." table)
-      (modify-syntax-entry ?- "." table)
-      (modify-syntax-entry ?/ "." table)
-      (modify-syntax-entry ?: "." table)
-      (modify-syntax-entry ?< "." table)
-      (modify-syntax-entry ?= "." table)
-      (modify-syntax-entry ?> "." table)
-      (modify-syntax-entry ?\\ "\\" table)
-      (modify-syntax-entry ?_ "_" table)
-      (modify-syntax-entry ?| "." table)
-      (modify-syntax-entry ?^ "'" table)
-
-      ;; Pseudo bit-syntax: Latin1 double angle quotes as parens.
-      ;;(modify-syntax-entry ?\253 "(?\273" table)
-      ;;(modify-syntax-entry ?\273 ")?\253" table)
-
-      (setq erlang-mode-syntax-table table))
-  "Syntax table in use in erlang-ts-mode buffers.")
-
-(defvar erlang-ts-mode-abbrev-table nil
-  "Abbrev table in use in erlang-ts-mode buffers.")
+(defvar trerl-mode-abbrev-table nil
+  "Abbrev table in use in trerl-mode buffers.")
 
 ;;;###autoload
 (define-derived-mode erlang-ts-mode prog-mode "Erlang"
   "Major mode for editing Erlang, powered by tree-sitter."
-  :group 'erlang-ts
-  ; :syntax-table erlang-ts-mode-syntax-table
-  :abbrev-table erlang-ts-mode-abbrev-table
-  (erlang-ts-setup))
+  :group 'trerl
+  :abbrev-table trerl-mode-abbrev-table
+  (when (treesit-ready-p 'erlang)
+    (treesit-parser-create 'erlang)
+    (trerl-setup)))
 
 ;;;###autoload
 (dolist (r '("\\.erl$" "\\.app\\.src$" "\\.escript"
              "\\.hrl$" "\\.xrl$" "\\.yrl" "/ebin/.+\\.app"))
   (add-to-list 'auto-mode-alist (cons r 'erlang-ts-mode)))
 
-(provide 'erlang-ts-mode)
+(provide 'trerl-mode)
+;;; trerl-mode.el ends here
