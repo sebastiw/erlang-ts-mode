6ebe70756627f7faece6a9ffbb298686ba4b9ec0
mats cronqvist <masse@cronqvi.st>
2024-07-01 17:21:30 +0200

    fontlock

--- erlang-ts-fontlock.el
+++ erlang-ts-fontlock.el
@@ -10,26 +10,31 @@
 
 (defun erlang-ts-fontlock-features ()
   "A `treesit-font-lock-feature-list'."
-  '((comment string doc) ; level 1
-    (function-name keyword type builtin constant) ; level 2
-    (variable-name string-interpolation key))) ; level 3
+  '((comment string atom variable) ; level 1
+    (function-name keyword boolean bitwise math) ; level 2
+    (call-remote call-local))) ; level 3
+
+(defmacro etsf--rule (name query)
+  "NAME QUERY FACE."
+  `(quote (:language erlang
+     :override t
+     :feature ,name
+     ,query)))
 
 (defvar erlang-ts-fontlock-rules
-  '(:language erlang :override t :feature
-              comment
-              ((comment) @font-lock-comment-face)
-              :language erlang :override t :feature
-              string
-              ((string) @font-lock-string-face)
-              :language erlang :override t :feature
-              variable-name
-              ((var) @font-lock-variable-name-face)
-              :language erlang :override nil :feature
-              keyword
-              (["case" "of" "end" "when"] @font-lock-keyword-face)
-              :language erlang :override nil :feature
-              constant
-              ((atom) @font-lock-constant-face)))
+  (append
+   (etsf--rule comment ((comment) @font-lock-comment-face))
+   (etsf--rule string ((string) @font-lock-string-face))
+   (etsf--rule atom ((atom) @font-lock-constant-face))
+   (etsf--rule variable ((var) @font-lock-variable-name-face))
+   (etsf--rule function-name ((function_clause (atom) @font-lock-function-name-face)))
+   (etsf--rule call-remote ((call (remote) @font-lock-function-call-face)))
+   (etsf--rule call-local ((call (atom) @font-lock-function-call-face)))
+   (etsf--rule keyword (["after" "begin" "case" "catch" "else" "end" "fun" "if" "maybe" "of" "receive" "try" "when"] @font-lock-keyword-face))
+   (etsf--rule boolean (["and" "andalso" "not" "or" "orelse" "xor"] @font-lock-builtin-face))
+   (etsf--rule bitwise (["band" "bnot" "bor" "bsl" "bsr" "bxor"] @font-lock-builtin-face))
+   (etsf--rule math (["div" "rem"] @font-lock-builtin-face)))
+  "A list of `font-lock-rule'.")
 
 (defun erlang-ts-fontlock ()
   "Return a `treesit-font-lock-settings."
